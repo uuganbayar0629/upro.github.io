fa51577596b5ef067c669f4a43c3c7a41124b939
uuganbayar0629 <50198013+uuganbayar0629@users.noreply.github.com>
2025-01-29 13:24:58 +1100

    edit final home page

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -7,11 +7,11 @@ import {Component, HostListener} from '@angular/core';
 })
 export class TimelineComponent {
   works: any[] = [
-    { name: 'Upwork', tooltip: 'Upwork platform', role: 'Fullstack Developer', beginYear: '2025', endYear: 'Present', logo: 'assets/profile.jpg' },
-    { name: 'AshidCapital LLC', tooltip: 'Ашид капитал ББСБ', role: 'Head of IT department', beginYear: '2023', endYear: '2025', logo: 'assets/profile.jpg' },
-    { name: 'ITC', tooltip: 'Гааль, татвар, санхүүгийн мэдээллийн технологийн төв', role: 'IT Manager', beginYear: '2021', endYear: '2023', logo: 'assets/profile.jpg' },
-    { name: 'ITC', tooltip: 'Гааль, татвар, санхүүгийн мэдээллийн технологийн төв', role: 'Senior Developer', beginYear: '2019', endYear: '2021', logo: 'assets/profile.jpg' },
-    { name: 'Interactive BI LLC', tooltip: 'Интерактив Би Ай ХХК', role: 'Developer', beginYear: '2015', endYear: '2019', logo: 'assets/profile.jpg' },
+    { name: 'Upwork', tooltip: 'Upwork platform', role: 'Fullstack Developer', beginYear: '2025', endYear: 'Present', logo: 'assets/logo/upwork.png' },
+    { name: 'AshidCapital LLC', tooltip: 'Ашид капитал ББСБ', role: 'Head of IT department', beginYear: '2023', endYear: '2025', logo: 'assets/logo/simple.png' },
+    { name: 'ITC', tooltip: 'Гааль, татвар, санхүүгийн мэдээллийн технологийн төв', role: 'IT Manager', beginYear: '2021', endYear: '2023', logo: 'assets/logo/ebarimt.png' },
+    { name: 'ITC', tooltip: 'Гааль, татвар, санхүүгийн мэдээллийн технологийн төв', role: 'Senior Developer', beginYear: '2019', endYear: '2021', logo: 'assets/logo/ebarimt.png' },
+    { name: 'Interactive BI LLC', tooltip: 'Интерактив Би Ай ХХК', role: 'Developer', beginYear: '2015', endYear: '2019', logo: 'assets/logo/ibi.png' },
   ];
 
   screenSize: 'lg' | 'md' | 'sm' = 'lg';
