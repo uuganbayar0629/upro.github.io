01b1a32a99abd556deb0f0f93d70c813c0090840
uuganbayar0629 <50198013+uuganbayar0629@users.noreply.github.com>
2025-01-31 10:08:16 +1100

    full route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import {HomeComponent} from "./home/home.component";
 import {ArticlesComponent} from "./articles/articles.component";
 import {ProjectsComponent} from "./projects/projects.component";
 import {ArticleDetailComponent} from "./article-detail/article-detail.component";
-import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
 const routes: Routes = [
-  { path: '', component:HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component:HomeComponent },
   { path: 'about', component:AboutComponent },
   { path: 'articles', component:ArticlesComponent },
   { path: 'article/detail', component:ArticleDetailComponent },
@@ -17,7 +17,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
